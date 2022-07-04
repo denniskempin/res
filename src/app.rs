db6c98de60b2b433416742832b8d39f24aabfd54
Dennis Kempin <dennis.kempin@gmail.com>
2022-07-03 17:45:11 -0700

    Switch to render with textures.
    Not as pretty since we cannot control how textures are interpolated.
    
    Might be able to use a custom glow component to specify a pixel program
    to render the texture.

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,21 @@
 use std::fs::File;
 use std::io::Read;
 
-use egui::vec2;
+use egui::ColorImage;
+use egui::Image;
+use egui::TextureHandle;
 use egui::Color32;
 use egui::DroppedFile;
 use egui::InputState;
 use egui::Key;
-use egui::Rect;
 use egui::Sense;
-use egui::Stroke;
 
 use crate::chip8::Chip8;
+use crate::chip8::State;
 
 pub struct EmulatorApp {
     emulator: Chip8,
-    pixels: [[u8; 64]; 32],
+    texture: TextureHandle,
 }
 
 impl EmulatorApp {
@@ -23,7 +24,7 @@ impl EmulatorApp {
         cc.egui_ctx.set_visuals(egui::Visuals::dark());
         EmulatorApp {
             emulator: Chip8::with_program(include_bytes!("ibm_logo.ch8")),
-            pixels: [[0u8; 64]; 32],
+            texture: cc.egui_ctx.load_texture("Framebuffer", ColorImage::example()),
         }
     }
 
@@ -31,35 +32,8 @@ impl EmulatorApp {
         let desired_size = ui.available_size();
         let (whole_rect, _) =
             ui.allocate_exact_size(desired_size, Sense::focusable_noninteractive());
-
-        // Create fading effect by adding/substracting from internal buffer of
-        // pixel brightness.
-        let new_pixels = &self.emulator.display.pixels;
-        for (y, row) in new_pixels.iter().enumerate() {
-            for (x, pixel) in row.iter().enumerate() {
-                if *pixel {
-                    self.pixels[y][x] = self.pixels[y][x].saturating_add(128);
-                } else {
-                    self.pixels[y][x] = self.pixels[y][x].saturating_sub(16);
-                }
-            }
-        }
-
-        // Draw pixels as rects
-        let stroke = Stroke::new(0.5, Color32::from_gray(80));
-        let pixel_width = whole_rect.width() / 64.0;
-        let pixel_height = whole_rect.height() / 32.0;
-        if ui.is_rect_visible(whole_rect) {
-            for (y, row) in self.pixels.iter().enumerate() {
-                for (x, pixel) in row.iter().enumerate() {
-                    let min =
-                        whole_rect.min + vec2(x as f32 * pixel_width, y as f32 * pixel_height);
-                    let pixel_rect = Rect::from_min_size(min, vec2(pixel_width, pixel_height));
-                    let color = Color32::from_gray(*pixel);
-                    ui.painter().rect(pixel_rect, 0.0, color, stroke)
-                }
-            }
-        }
+        let image = Image::new(&self.texture, self.texture.size_vec2());
+        image.paint_at(ui, whole_rect);
     }
 
     fn load_dropped_file(&mut self, drop: &DroppedFile) {
@@ -94,6 +68,21 @@ impl EmulatorApp {
         keys[0xE] = input.key_down(Key::E);
         keys[0xF] = input.key_down(Key::F);
     }
+
+    fn update_display(&mut self) {
+        let mut image = ColorImage::new([64, 32], Color32::BLUE);
+        for (y, row) in self.emulator.display.pixels.iter().enumerate() {
+            for (x, pixel) in row.iter().enumerate() {
+                if *pixel {
+                    image.pixels[y * row.len() + x] = Color32::WHITE;
+                } else {
+                    image.pixels[y * row.len() + x] = Color32::BLACK;
+                }
+            }
+        }
+        self.texture.set(image);
+
+    }
 }
 
 impl eframe::App for EmulatorApp {
@@ -107,7 +96,9 @@ impl eframe::App for EmulatorApp {
         // egui is rendering at 60Hz, Chip8 runs at 500Hz, so we need to run
         // 8-ish cpu cycles for each frame.
         for _ in 0..8 {
-            self.emulator.emulate_tick().unwrap();
+            if let State::DisplayUpdated = self.emulator.emulate_tick().unwrap() {
+                self.update_display();
+            }
         }
 
         // Render emulator display
