137f28b5b7d3648a1b09a29ccb43ad75dc53ba3c
Dennis Kempin <dennis.kempin@gmail.com>
2022-11-26 21:39:44 -0800

    Prettified check-all and add benchmark runs

--- check-all.sh
+++ check-all.sh
@@ -1,7 +1,16 @@
-set -ex
+set -e
 
+printf '\e[1m\e[32m---------------------------- Tests -------------------------------------\e[0m\n'
 cargo nextest run
-cargo criterion
+echo
+printf '\e[1m\e[32m-------------------------- Benchmarks ----------------------------------\e[0m\n'
+cargo criterion --plotting-backend disabled -- --sample-size 10 --measurement-time 1 --warm-up-time 1
+echo
+printf '\e[1m\e[32m---------------------------- Clippy ------------------------------------\e[0m\n'
 cargo clippy
-cargo +nightly fmt --check
+echo
+printf '\e[1m\e[32m---------------------------- Format ------------------------------------\e[0m\n'
+cargo +nightly fmt --check && echo "ok"
+echo
+printf '\e[1m\e[32m-------------------------- Wasm Build ----------------------------------\e[0m\n'
 yarn build
